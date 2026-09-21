2daeb16e3a21e87a2aa6e34ee70ce203f5f93e5a
Sinonimia Deploy <deploy@noreply.localhost>
2026-09-21 22:22:10 +0200

    Add translated English synonymy batch 29

--- js/dictionary-manifest.js
+++ js/dictionary-manifest.js
@@ -12,10 +12,10 @@
  */
 window.SINONIMIA_DICTIONARY_SHARDS = {
   es: [
-    { file: "js/data.es.js", src: "js/data.es.js?v=1ae6b6fc9a" },
-    { file: "js/data.es.2.js", src: "js/data.es.2.js?v=327dd60271" },
+    { file: "js/data.es.js", src: "js/data.es.js?v=dfa99b5db8" },
+    { file: "js/data.es.2.js", src: "js/data.es.2.js?v=d7059162ac" },
   ],
   en: [
-    { file: "js/data.en.js", src: "js/data.en.js?v=dbe2193aa8" },
+    { file: "js/data.en.js", src: "js/data.en.js?v=06033e74eb" },
   ],
 };
