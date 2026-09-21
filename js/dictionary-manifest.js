1b4b2545aca12dbe4828e7b52bda1b04b61be094
Sinonimia Deploy <deploy@noreply.localhost>
2026-09-21 20:50:37 +0200

    Add translated English synonymy batch 19

--- js/dictionary-manifest.js
+++ js/dictionary-manifest.js
@@ -12,10 +12,10 @@
  */
 window.SINONIMIA_DICTIONARY_SHARDS = {
   es: [
-    { file: "js/data.es.js", src: "js/data.es.js?v=1f380ad45f" },
+    { file: "js/data.es.js", src: "js/data.es.js?v=c10a4756b1" },
     { file: "js/data.es.2.js", src: "js/data.es.2.js?v=327dd60271" },
   ],
   en: [
-    { file: "js/data.en.js", src: "js/data.en.js?v=5e54ba55d7" },
+    { file: "js/data.en.js", src: "js/data.en.js?v=797c71daff" },
   ],
 };
