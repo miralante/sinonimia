db185c69c1bbe7f5aa40d204b4be1bf5c208c62c
Sinonimia Deploy <deploy@noreply.localhost>
2026-09-21 19:39:46 +0200

    Add translated English synonymy batch 11

--- js/dictionary-manifest.js
+++ js/dictionary-manifest.js
@@ -12,10 +12,10 @@
  */
 window.SINONIMIA_DICTIONARY_SHARDS = {
   es: [
-    { file: "js/data.es.js", src: "js/data.es.js?v=a955727663" },
+    { file: "js/data.es.js", src: "js/data.es.js?v=3de1ed5d07" },
     { file: "js/data.es.2.js", src: "js/data.es.2.js?v=a5a66e79ca" },
   ],
   en: [
-    { file: "js/data.en.js", src: "js/data.en.js?v=4f13e0dc7b" },
+    { file: "js/data.en.js", src: "js/data.en.js?v=234ce4202d" },
   ],
 };
