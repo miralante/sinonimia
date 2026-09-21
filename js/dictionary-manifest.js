ff994605a424ded3d9f363f03d82a4fade32a24e
Sinonimia Deploy <deploy@noreply.localhost>
2026-09-21 23:36:42 +0200

    Add translated English synonymy batch 37

--- js/dictionary-manifest.js
+++ js/dictionary-manifest.js
@@ -12,10 +12,10 @@
  */
 window.SINONIMIA_DICTIONARY_SHARDS = {
   es: [
-    { file: "js/data.es.js", src: "js/data.es.js?v=83c2b1e441" },
-    { file: "js/data.es.2.js", src: "js/data.es.2.js?v=aa146103b4" },
+    { file: "js/data.es.js", src: "js/data.es.js?v=edc3ae1ec9" },
+    { file: "js/data.es.2.js", src: "js/data.es.2.js?v=f588180337" },
   ],
   en: [
-    { file: "js/data.en.js", src: "js/data.en.js?v=e5fd3c233b" },
+    { file: "js/data.en.js", src: "js/data.en.js?v=04907c64e2" },
   ],
 };
