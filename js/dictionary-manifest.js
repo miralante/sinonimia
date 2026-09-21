b863cd17635c60239f5f61ca2d2321cc4f737132
Sinonimia Deploy <deploy@noreply.localhost>
2026-09-22 01:59:08 +0200

    Add translated English synonymy batch 56

--- js/dictionary-manifest.js
+++ js/dictionary-manifest.js
@@ -12,10 +12,10 @@
  */
 window.SINONIMIA_DICTIONARY_SHARDS = {
   es: [
-    { file: "js/data.es.js", src: "js/data.es.js?v=414c1a5d43" },
+    { file: "js/data.es.js", src: "js/data.es.js?v=fe7a6045a6" },
     { file: "js/data.es.2.js", src: "js/data.es.2.js?v=01d665a4c8" },
   ],
   en: [
-    { file: "js/data.en.js", src: "js/data.en.js?v=eb4a38a3a2" },
+    { file: "js/data.en.js", src: "js/data.en.js?v=9bfb9758a7" },
   ],
 };
