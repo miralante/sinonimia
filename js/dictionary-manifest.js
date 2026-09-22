5d6ced1b93d55a50ee9975bf0b556684485c4541
Sinonimia Deploy <deploy@noreply.localhost>
2026-09-22 14:12:40 +0200

    Add translated English synonymy batch 127

--- js/dictionary-manifest.js
+++ js/dictionary-manifest.js
@@ -12,10 +12,10 @@
  */
 window.SINONIMIA_DICTIONARY_SHARDS = {
   es: [
-    { file: "js/data.es.js", src: "js/data.es.js?v=6b6b8d8be1" },
+    { file: "js/data.es.js", src: "js/data.es.js?v=1274611f1c" },
     { file: "js/data.es.2.js", src: "js/data.es.2.js?v=01d665a4c8" },
   ],
   en: [
-    { file: "js/data.en.js", src: "js/data.en.js?v=6a5aa8dd16" },
+    { file: "js/data.en.js", src: "js/data.en.js?v=820e0f130c" },
   ],
 };
