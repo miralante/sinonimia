12268cebbd24cd11609ddb8dbd5296838583eed6
Sinonimia Deploy <deploy@noreply.localhost>
2026-09-22 04:32:47 +0200

    Add translated English synonymy batch 73

--- js/dictionary-manifest.js
+++ js/dictionary-manifest.js
@@ -12,10 +12,10 @@
  */
 window.SINONIMIA_DICTIONARY_SHARDS = {
   es: [
-    { file: "js/data.es.js", src: "js/data.es.js?v=043c98076e" },
+    { file: "js/data.es.js", src: "js/data.es.js?v=1e8f037c1f" },
     { file: "js/data.es.2.js", src: "js/data.es.2.js?v=01d665a4c8" },
   ],
   en: [
-    { file: "js/data.en.js", src: "js/data.en.js?v=ea843026a7" },
+    { file: "js/data.en.js", src: "js/data.en.js?v=53f6ea89c0" },
   ],
 };
