3c71a1dc292f958ff15a47b8fb42462fc6ac9551
Sinonimia Deploy <deploy@noreply.localhost>
2026-09-22 12:08:12 +0200

    Add translated English synonymy batch 120

--- js/dictionary-manifest.js
+++ js/dictionary-manifest.js
@@ -12,10 +12,10 @@
  */
 window.SINONIMIA_DICTIONARY_SHARDS = {
   es: [
-    { file: "js/data.es.js", src: "js/data.es.js?v=b8a00187e1" },
+    { file: "js/data.es.js", src: "js/data.es.js?v=e6356a9ca7" },
     { file: "js/data.es.2.js", src: "js/data.es.2.js?v=01d665a4c8" },
   ],
   en: [
-    { file: "js/data.en.js", src: "js/data.en.js?v=2bc1868624" },
+    { file: "js/data.en.js", src: "js/data.en.js?v=f4b46956fc" },
   ],
 };
