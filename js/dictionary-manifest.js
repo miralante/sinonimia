af3235ebfab223bcd0c562f67df0c28f34565931
Sinonimia Deploy <deploy@noreply.localhost>
2026-09-22 03:48:03 +0200

    Add translated English synonymy batch 68

--- js/dictionary-manifest.js
+++ js/dictionary-manifest.js
@@ -12,10 +12,10 @@
  */
 window.SINONIMIA_DICTIONARY_SHARDS = {
   es: [
-    { file: "js/data.es.js", src: "js/data.es.js?v=1e5a01a349" },
+    { file: "js/data.es.js", src: "js/data.es.js?v=eaad9bc85f" },
     { file: "js/data.es.2.js", src: "js/data.es.2.js?v=01d665a4c8" },
   ],
   en: [
-    { file: "js/data.en.js", src: "js/data.en.js?v=3f6eda5323" },
+    { file: "js/data.en.js", src: "js/data.en.js?v=88389c406e" },
   ],
 };
