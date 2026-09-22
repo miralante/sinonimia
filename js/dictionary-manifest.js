850d3be40fc6b2c0665b6de6bb1c3848883d7500
Sinonimia Deploy <deploy@noreply.localhost>
2026-09-22 04:59:01 +0200

    Add translated English synonymy batch 76

--- js/dictionary-manifest.js
+++ js/dictionary-manifest.js
@@ -12,10 +12,10 @@
  */
 window.SINONIMIA_DICTIONARY_SHARDS = {
   es: [
-    { file: "js/data.es.js", src: "js/data.es.js?v=96fbd3bbc8" },
+    { file: "js/data.es.js", src: "js/data.es.js?v=a5ee57296a" },
     { file: "js/data.es.2.js", src: "js/data.es.2.js?v=01d665a4c8" },
   ],
   en: [
-    { file: "js/data.en.js", src: "js/data.en.js?v=abb2b329fd" },
+    { file: "js/data.en.js", src: "js/data.en.js?v=154cf79f88" },
   ],
 };
