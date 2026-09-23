c0e23f3bd36070a5d2525c9338bf5908297334ea
Sinonimia Deploy <deploy@noreply.localhost>
2026-09-23 20:05:02 +0200

    Continue English synonymy batch 291

--- js/dictionary-manifest.js
+++ js/dictionary-manifest.js
@@ -12,11 +12,11 @@
  */
 window.SINONIMIA_DICTIONARY_SHARDS = {
   es: [
-    { file: "js/data.es.js", src: "js/data.es.js?v=9eb02bae5d" },
-    { file: "js/data.es.2.js", src: "js/data.es.2.js?v=7123caec31" },
+    { file: "js/data.es.js", src: "js/data.es.js?v=168ba60334" },
+    { file: "js/data.es.2.js", src: "js/data.es.2.js?v=7ff2ce3fee" },
   ],
   en: [
     { file: "js/data.en.js", src: "js/data.en.js?v=6dfdbdccee" },
-    { file: "js/data.en.2.js", src: "js/data.en.2.js?v=5570ba6e36" },
+    { file: "js/data.en.2.js", src: "js/data.en.2.js?v=5b9b1c609b" },
   ],
 };
