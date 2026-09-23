43f3d8854ebf0d08ac1cb8dc76c1c62590c499f7
Sinonimia Deploy <deploy@noreply.localhost>
2026-09-23 15:42:58 +0200

    Add polysemous English synonymy batch 269

--- js/dictionary-manifest.js
+++ js/dictionary-manifest.js
@@ -12,11 +12,11 @@
  */
 window.SINONIMIA_DICTIONARY_SHARDS = {
   es: [
-    { file: "js/data.es.js", src: "js/data.es.js?v=a628fceb2e" },
-    { file: "js/data.es.2.js", src: "js/data.es.2.js?v=d3f7736171" },
+    { file: "js/data.es.js", src: "js/data.es.js?v=8c7343caa1" },
+    { file: "js/data.es.2.js", src: "js/data.es.2.js?v=8a779b0c9d" },
   ],
   en: [
     { file: "js/data.en.js", src: "js/data.en.js?v=6dfdbdccee" },
-    { file: "js/data.en.2.js", src: "js/data.en.2.js?v=ce46889bdc" },
+    { file: "js/data.en.2.js", src: "js/data.en.2.js?v=6bd8eae7ba" },
   ],
 };
