06e734bea33e0dda6f133a50c5eda3aa43ab133e
Sinonimia Deploy <deploy@noreply.localhost>
2026-09-23 23:08:47 +0200

    Continue English synonymy batch 301

--- js/dictionary-manifest.js
+++ js/dictionary-manifest.js
@@ -12,11 +12,11 @@
  */
 window.SINONIMIA_DICTIONARY_SHARDS = {
   es: [
-    { file: "js/data.es.js", src: "js/data.es.js?v=d895773523" },
-    { file: "js/data.es.2.js", src: "js/data.es.2.js?v=5b4814c7d3" },
+    { file: "js/data.es.js", src: "js/data.es.js?v=0c8a6cbff0" },
+    { file: "js/data.es.2.js", src: "js/data.es.2.js?v=272645b839" },
   ],
   en: [
     { file: "js/data.en.js", src: "js/data.en.js?v=6dfdbdccee" },
-    { file: "js/data.en.2.js", src: "js/data.en.2.js?v=83b09ad9ee" },
+    { file: "js/data.en.2.js", src: "js/data.en.2.js?v=8b29867c1a" },
   ],
 };
