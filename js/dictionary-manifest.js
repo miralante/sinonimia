a587f040ebcdfe4389e4dbe10d93461edd01c133
Sinonimia Deploy <deploy@noreply.localhost>
2026-09-23 19:23:08 +0200

    Rebalance Spanish shards after English synonymy batch 289

--- js/dictionary-manifest.js
+++ js/dictionary-manifest.js
@@ -12,11 +12,11 @@
  */
 window.SINONIMIA_DICTIONARY_SHARDS = {
   es: [
-    { file: "js/data.es.js", src: "js/data.es.js?v=60cc2df438" },
-    { file: "js/data.es.2.js", src: "js/data.es.2.js?v=ad7c3fd38e" },
+    { file: "js/data.es.js", src: "js/data.es.js?v=2f4741e556" },
+    { file: "js/data.es.2.js", src: "js/data.es.2.js?v=ab5180570e" },
   ],
   en: [
     { file: "js/data.en.js", src: "js/data.en.js?v=6dfdbdccee" },
-    { file: "js/data.en.2.js", src: "js/data.en.2.js?v=6803b1d811" },
+    { file: "js/data.en.2.js", src: "js/data.en.2.js?v=1e56bfe39a" },
   ],
 };
