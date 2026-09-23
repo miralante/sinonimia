968d95109ceff15d68473582566d0e533d866d30
Sinonimia Deploy <deploy@noreply.localhost>
2026-09-23 09:24:54 +0200

    Add polysemous English synonymy batch 244

--- js/dictionary-manifest.js
+++ js/dictionary-manifest.js
@@ -12,10 +12,10 @@
  */
 window.SINONIMIA_DICTIONARY_SHARDS = {
   es: [
-    { file: "js/data.es.js", src: "js/data.es.js?v=ae1355febb" },
+    { file: "js/data.es.js", src: "js/data.es.js?v=7f395bc241" },
     { file: "js/data.es.2.js", src: "js/data.es.2.js?v=d1b33f09c4" },
   ],
   en: [
-    { file: "js/data.en.js", src: "js/data.en.js?v=34e03045ee" },
+    { file: "js/data.en.js", src: "js/data.en.js?v=9e70c4220c" },
   ],
 };
