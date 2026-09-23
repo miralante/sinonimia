7be64e1678946a31b68c22a5d77b7232dc55e234
Sinonimia Deploy <deploy@noreply.localhost>
2026-09-23 16:10:16 +0200

    Add polysemous English synonymy batch 272

--- js/dictionary-manifest.js
+++ js/dictionary-manifest.js
@@ -12,11 +12,11 @@
  */
 window.SINONIMIA_DICTIONARY_SHARDS = {
   es: [
-    { file: "js/data.es.js", src: "js/data.es.js?v=f85892f7cd" },
-    { file: "js/data.es.2.js", src: "js/data.es.2.js?v=a07bf81207" },
+    { file: "js/data.es.js", src: "js/data.es.js?v=9ec6610421" },
+    { file: "js/data.es.2.js", src: "js/data.es.2.js?v=1a2f350491" },
   ],
   en: [
     { file: "js/data.en.js", src: "js/data.en.js?v=6dfdbdccee" },
-    { file: "js/data.en.2.js", src: "js/data.en.2.js?v=4a818260cb" },
+    { file: "js/data.en.2.js", src: "js/data.en.2.js?v=2297f362a5" },
   ],
 };
