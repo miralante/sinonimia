b40662190d960db39cdf2b67da7c71440d95ee0c
Sinonimia Deploy <deploy@noreply.localhost>
2026-09-23 21:16:07 +0200

    Continue English synonymy batch 295

--- js/dictionary-manifest.js
+++ js/dictionary-manifest.js
@@ -12,11 +12,11 @@
  */
 window.SINONIMIA_DICTIONARY_SHARDS = {
   es: [
-    { file: "js/data.es.js", src: "js/data.es.js?v=cf3c9b0611" },
-    { file: "js/data.es.2.js", src: "js/data.es.2.js?v=2fab0a6962" },
+    { file: "js/data.es.js", src: "js/data.es.js?v=893ede00e2" },
+    { file: "js/data.es.2.js", src: "js/data.es.2.js?v=a9ea91fdf8" },
   ],
   en: [
     { file: "js/data.en.js", src: "js/data.en.js?v=6dfdbdccee" },
-    { file: "js/data.en.2.js", src: "js/data.en.2.js?v=44da05cab9" },
+    { file: "js/data.en.2.js", src: "js/data.en.2.js?v=e2fe3ff6c2" },
   ],
 };
