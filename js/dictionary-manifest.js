9503a92f21011017adb5614d52abe1bfee3ee724
Sinonimia Deploy <deploy@noreply.localhost>
2026-09-23 21:03:43 +0200

    Continue English synonymy batch 294

--- js/dictionary-manifest.js
+++ js/dictionary-manifest.js
@@ -12,11 +12,11 @@
  */
 window.SINONIMIA_DICTIONARY_SHARDS = {
   es: [
-    { file: "js/data.es.js", src: "js/data.es.js?v=3f07079519" },
-    { file: "js/data.es.2.js", src: "js/data.es.2.js?v=3652ab80b7" },
+    { file: "js/data.es.js", src: "js/data.es.js?v=cf3c9b0611" },
+    { file: "js/data.es.2.js", src: "js/data.es.2.js?v=2fab0a6962" },
   ],
   en: [
     { file: "js/data.en.js", src: "js/data.en.js?v=6dfdbdccee" },
-    { file: "js/data.en.2.js", src: "js/data.en.2.js?v=e8df2875e7" },
+    { file: "js/data.en.2.js", src: "js/data.en.2.js?v=44da05cab9" },
   ],
 };
