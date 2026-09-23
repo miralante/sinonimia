a6bf17d55c833629a67c5ba9d13f9fb0c558140f
Sinonimia Deploy <deploy@noreply.localhost>
2026-09-23 09:03:51 +0200

    Add polysemous English synonymy batch 241

--- js/dictionary-manifest.js
+++ js/dictionary-manifest.js
@@ -12,10 +12,10 @@
  */
 window.SINONIMIA_DICTIONARY_SHARDS = {
   es: [
-    { file: "js/data.es.js", src: "js/data.es.js?v=f45be124e4" },
+    { file: "js/data.es.js", src: "js/data.es.js?v=62420033f1" },
     { file: "js/data.es.2.js", src: "js/data.es.2.js?v=5806f7eae2" },
   ],
   en: [
-    { file: "js/data.en.js", src: "js/data.en.js?v=52c027a46a" },
+    { file: "js/data.en.js", src: "js/data.en.js?v=d00e43ded3" },
   ],
 };
