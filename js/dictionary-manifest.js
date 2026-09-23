c0a8c30fa2ddee9750ec20fb293d5ed0a31fc474
Sinonimia Deploy <deploy@noreply.localhost>
2026-09-23 09:10:09 +0200

    Add polysemous English synonymy batch 242

--- js/dictionary-manifest.js
+++ js/dictionary-manifest.js
@@ -12,10 +12,10 @@
  */
 window.SINONIMIA_DICTIONARY_SHARDS = {
   es: [
-    { file: "js/data.es.js", src: "js/data.es.js?v=62420033f1" },
+    { file: "js/data.es.js", src: "js/data.es.js?v=2c1c63c8e7" },
     { file: "js/data.es.2.js", src: "js/data.es.2.js?v=5806f7eae2" },
   ],
   en: [
-    { file: "js/data.en.js", src: "js/data.en.js?v=d00e43ded3" },
+    { file: "js/data.en.js", src: "js/data.en.js?v=4e1a6b93f2" },
   ],
 };
