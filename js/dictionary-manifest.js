0e1e911d5974dffb32b50e406361edf99d964899
Sinonimia Deploy <deploy@noreply.localhost>
2026-09-24 16:57:45 +0200

    Continue English synonymy batch 319

--- js/dictionary-manifest.js
+++ js/dictionary-manifest.js
@@ -12,11 +12,11 @@
  */
 window.SINONIMIA_DICTIONARY_SHARDS = {
   es: [
-    { file: "js/data.es.js", src: "js/data.es.js?v=284a2e4ec8" },
-    { file: "js/data.es.2.js", src: "js/data.es.2.js?v=f6e094b41a" },
+    { file: "js/data.es.js", src: "js/data.es.js?v=5c8bf7b570" },
+    { file: "js/data.es.2.js", src: "js/data.es.2.js?v=9455b93ffd" },
   ],
   en: [
     { file: "js/data.en.js", src: "js/data.en.js?v=6dfdbdccee" },
-    { file: "js/data.en.2.js", src: "js/data.en.2.js?v=2e05328785" },
+    { file: "js/data.en.2.js", src: "js/data.en.2.js?v=31ee84227b" },
   ],
 };
