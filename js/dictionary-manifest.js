3c3dfb6762fe5db6cf902d219ebeb612c5ddb788
Sinonimia Deploy <deploy@noreply.localhost>
2026-09-24 15:46:38 +0200

    Continue English synonymy batch 308

--- js/dictionary-manifest.js
+++ js/dictionary-manifest.js
@@ -12,11 +12,11 @@
  */
 window.SINONIMIA_DICTIONARY_SHARDS = {
   es: [
-    { file: "js/data.es.js", src: "js/data.es.js?v=e8629b8b02" },
-    { file: "js/data.es.2.js", src: "js/data.es.2.js?v=72dedd3a96" },
+    { file: "js/data.es.js", src: "js/data.es.js?v=74019bc6f9" },
+    { file: "js/data.es.2.js", src: "js/data.es.2.js?v=1626b12b25" },
   ],
   en: [
     { file: "js/data.en.js", src: "js/data.en.js?v=6dfdbdccee" },
-    { file: "js/data.en.2.js", src: "js/data.en.2.js?v=c254af1ed8" },
+    { file: "js/data.en.2.js", src: "js/data.en.2.js?v=72b349cc9e" },
   ],
 };
