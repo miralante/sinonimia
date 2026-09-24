60bf633a32d15c59d70c3dc59236fee64918ee70
Sinonimia Deploy <deploy@noreply.localhost>
2026-09-24 16:19:10 +0200

    Continue English synonymy batch 313

--- js/dictionary-manifest.js
+++ js/dictionary-manifest.js
@@ -12,11 +12,11 @@
  */
 window.SINONIMIA_DICTIONARY_SHARDS = {
   es: [
-    { file: "js/data.es.js", src: "js/data.es.js?v=d7e8d4c286" },
-    { file: "js/data.es.2.js", src: "js/data.es.2.js?v=51ec608973" },
+    { file: "js/data.es.js", src: "js/data.es.js?v=1724b43353" },
+    { file: "js/data.es.2.js", src: "js/data.es.2.js?v=e1d6a3e01a" },
   ],
   en: [
     { file: "js/data.en.js", src: "js/data.en.js?v=6dfdbdccee" },
-    { file: "js/data.en.2.js", src: "js/data.en.2.js?v=b698f64603" },
+    { file: "js/data.en.2.js", src: "js/data.en.2.js?v=e35a5ad34c" },
   ],
 };
