ad2c96122a1f609e865aeb6a0fd64f365a62c07b
Sinonimia Deploy <deploy@noreply.localhost>
2026-09-24 19:11:14 +0200

    Continue English synonymy batch 330

--- js/dictionary-manifest.js
+++ js/dictionary-manifest.js
@@ -12,11 +12,11 @@
  */
 window.SINONIMIA_DICTIONARY_SHARDS = {
   es: [
-    { file: "js/data.es.js", src: "js/data.es.js?v=3c8ce1cf29" },
-    { file: "js/data.es.2.js", src: "js/data.es.2.js?v=94f457c7d9" },
+    { file: "js/data.es.js", src: "js/data.es.js?v=638824b5fe" },
+    { file: "js/data.es.2.js", src: "js/data.es.2.js?v=39dc081963" },
   ],
   en: [
     { file: "js/data.en.js", src: "js/data.en.js?v=6dfdbdccee" },
-    { file: "js/data.en.2.js", src: "js/data.en.2.js?v=291b0e89a8" },
+    { file: "js/data.en.2.js", src: "js/data.en.2.js?v=e338968b90" },
   ],
 };
