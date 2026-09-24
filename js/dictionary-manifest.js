1f364e23716f30b505b784ca5e9c75b6ae240f01
Sinonimia Deploy <deploy@noreply.localhost>
2026-09-24 17:52:19 +0200

    Continue English synonymy batch 327

--- js/dictionary-manifest.js
+++ js/dictionary-manifest.js
@@ -12,11 +12,11 @@
  */
 window.SINONIMIA_DICTIONARY_SHARDS = {
   es: [
-    { file: "js/data.es.js", src: "js/data.es.js?v=b64a27db1b" },
-    { file: "js/data.es.2.js", src: "js/data.es.2.js?v=66589e753d" },
+    { file: "js/data.es.js", src: "js/data.es.js?v=dee5474914" },
+    { file: "js/data.es.2.js", src: "js/data.es.2.js?v=bb99ed7165" },
   ],
   en: [
     { file: "js/data.en.js", src: "js/data.en.js?v=6dfdbdccee" },
-    { file: "js/data.en.2.js", src: "js/data.en.2.js?v=de13aa29a9" },
+    { file: "js/data.en.2.js", src: "js/data.en.2.js?v=776103b15b" },
   ],
 };
