ea7faad69c153288e1676eabdc3a54c8b9b6af31
Sinonimia Deploy <deploy@noreply.localhost>
2026-09-24 16:11:23 +0200

    Continue English synonymy batch 312

--- js/dictionary-manifest.js
+++ js/dictionary-manifest.js
@@ -12,11 +12,11 @@
  */
 window.SINONIMIA_DICTIONARY_SHARDS = {
   es: [
-    { file: "js/data.es.js", src: "js/data.es.js?v=4faf893f31" },
-    { file: "js/data.es.2.js", src: "js/data.es.2.js?v=4735a8b7c7" },
+    { file: "js/data.es.js", src: "js/data.es.js?v=d7e8d4c286" },
+    { file: "js/data.es.2.js", src: "js/data.es.2.js?v=51ec608973" },
   ],
   en: [
     { file: "js/data.en.js", src: "js/data.en.js?v=6dfdbdccee" },
-    { file: "js/data.en.2.js", src: "js/data.en.2.js?v=a3d961e2f5" },
+    { file: "js/data.en.2.js", src: "js/data.en.2.js?v=b698f64603" },
   ],
 };
