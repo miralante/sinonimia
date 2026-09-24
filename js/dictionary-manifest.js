378235cbce9ca5896cae7714a205a0406c3ca639
Sinonimia Deploy <deploy@noreply.localhost>
2026-09-24 15:51:31 +0200

    Continue English synonymy batch 309

--- js/dictionary-manifest.js
+++ js/dictionary-manifest.js
@@ -12,11 +12,11 @@
  */
 window.SINONIMIA_DICTIONARY_SHARDS = {
   es: [
-    { file: "js/data.es.js", src: "js/data.es.js?v=74019bc6f9" },
-    { file: "js/data.es.2.js", src: "js/data.es.2.js?v=1626b12b25" },
+    { file: "js/data.es.js", src: "js/data.es.js?v=8cb43763d4" },
+    { file: "js/data.es.2.js", src: "js/data.es.2.js?v=47c25aec93" },
   ],
   en: [
     { file: "js/data.en.js", src: "js/data.en.js?v=6dfdbdccee" },
-    { file: "js/data.en.2.js", src: "js/data.en.2.js?v=72b349cc9e" },
+    { file: "js/data.en.2.js", src: "js/data.en.2.js?v=6abce37f35" },
   ],
 };
