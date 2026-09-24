76570e7489520f387d26fa97b4d75ba12dc5b0eb
Sinonimia Deploy <deploy@noreply.localhost>
2026-09-24 23:54:50 +0200

    Continue English synonymy batch 343

--- js/dictionary-manifest.js
+++ js/dictionary-manifest.js
@@ -12,11 +12,11 @@
  */
 window.SINONIMIA_DICTIONARY_SHARDS = {
   es: [
-    { file: "js/data.es.js", src: "js/data.es.js?v=bea387cea5" },
-    { file: "js/data.es.2.js", src: "js/data.es.2.js?v=4f1d2e3224" },
+    { file: "js/data.es.js", src: "js/data.es.js?v=311876cc33" },
+    { file: "js/data.es.2.js", src: "js/data.es.2.js?v=09e0a4e5e6" },
   ],
   en: [
     { file: "js/data.en.js", src: "js/data.en.js?v=6dfdbdccee" },
-    { file: "js/data.en.2.js", src: "js/data.en.2.js?v=357fb2edd4" },
+    { file: "js/data.en.2.js", src: "js/data.en.2.js?v=c15d83b4f8" },
   ],
 };
