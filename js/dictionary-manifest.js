fae1532a718a42c8c4e4702087be05fe66f1b322
Sinonimia Deploy <deploy@noreply.localhost>
2026-09-24 19:35:22 +0200

    Continue English synonymy batch 331

--- js/dictionary-manifest.js
+++ js/dictionary-manifest.js
@@ -12,11 +12,11 @@
  */
 window.SINONIMIA_DICTIONARY_SHARDS = {
   es: [
-    { file: "js/data.es.js", src: "js/data.es.js?v=638824b5fe" },
-    { file: "js/data.es.2.js", src: "js/data.es.2.js?v=39dc081963" },
+    { file: "js/data.es.js", src: "js/data.es.js?v=156ab53822" },
+    { file: "js/data.es.2.js", src: "js/data.es.2.js?v=a7270d181f" },
   ],
   en: [
     { file: "js/data.en.js", src: "js/data.en.js?v=6dfdbdccee" },
-    { file: "js/data.en.2.js", src: "js/data.en.2.js?v=e338968b90" },
+    { file: "js/data.en.2.js", src: "js/data.en.2.js?v=9c6e9864c0" },
   ],
 };
