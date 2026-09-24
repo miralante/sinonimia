991aaa7bc89e102f1474ea98be902583006121c4
Sinonimia Deploy <deploy@noreply.localhost>
2026-09-25 01:30:12 +0200

    Continue English synonymy batch 354

--- js/dictionary-manifest.js
+++ js/dictionary-manifest.js
@@ -12,11 +12,11 @@
  */
 window.SINONIMIA_DICTIONARY_SHARDS = {
   es: [
-    { file: "js/data.es.js", src: "js/data.es.js?v=a6c93dbe09" },
-    { file: "js/data.es.2.js", src: "js/data.es.2.js?v=f2d251d12e" },
+    { file: "js/data.es.js", src: "js/data.es.js?v=c0cdfca19c" },
+    { file: "js/data.es.2.js", src: "js/data.es.2.js?v=18a2fb35c3" },
   ],
   en: [
     { file: "js/data.en.js", src: "js/data.en.js?v=6dfdbdccee" },
-    { file: "js/data.en.2.js", src: "js/data.en.2.js?v=2a348545a9" },
+    { file: "js/data.en.2.js", src: "js/data.en.2.js?v=ac50bcff59" },
   ],
 };
