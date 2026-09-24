a382142f2d790b41a99b5e2097bf012483c9a7f3
Sinonimia Deploy <deploy@noreply.localhost>
2026-09-24 16:04:12 +0200

    Continue English synonymy batch 311

--- js/dictionary-manifest.js
+++ js/dictionary-manifest.js
@@ -12,11 +12,11 @@
  */
 window.SINONIMIA_DICTIONARY_SHARDS = {
   es: [
-    { file: "js/data.es.js", src: "js/data.es.js?v=0c5ce220b7" },
-    { file: "js/data.es.2.js", src: "js/data.es.2.js?v=6722cb49b3" },
+    { file: "js/data.es.js", src: "js/data.es.js?v=4faf893f31" },
+    { file: "js/data.es.2.js", src: "js/data.es.2.js?v=4735a8b7c7" },
   ],
   en: [
     { file: "js/data.en.js", src: "js/data.en.js?v=6dfdbdccee" },
-    { file: "js/data.en.2.js", src: "js/data.en.2.js?v=34ba357ff4" },
+    { file: "js/data.en.2.js", src: "js/data.en.2.js?v=a3d961e2f5" },
   ],
 };
