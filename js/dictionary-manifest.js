c181a257390d3b44d115988e952a51fe7e960322
Sinonimia Deploy <deploy@noreply.localhost>
2026-09-25 14:59:38 +0200

    Add reviewed English synonymy batch 378

--- js/dictionary-manifest.js
+++ js/dictionary-manifest.js
@@ -12,11 +12,11 @@
  */
 window.SINONIMIA_DICTIONARY_SHARDS = {
   es: [
-    { file: "js/data.es.js", src: "js/data.es.js?v=542583e522" },
-    { file: "js/data.es.2.js", src: "js/data.es.2.js?v=c1e65ac330" },
+    { file: "js/data.es.js", src: "js/data.es.js?v=70cebc0f05" },
+    { file: "js/data.es.2.js", src: "js/data.es.2.js?v=5bdd9de4fc" },
   ],
   en: [
     { file: "js/data.en.js", src: "js/data.en.js?v=6dfdbdccee" },
-    { file: "js/data.en.2.js", src: "js/data.en.2.js?v=d38868e3cc" },
+    { file: "js/data.en.2.js", src: "js/data.en.2.js?v=54a6342362" },
   ],
 };
