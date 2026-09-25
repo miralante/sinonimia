31da814f4636723659464ab8c7b1e3ed19059024
Sinonimia Deploy <deploy@noreply.localhost>
2026-09-25 10:15:33 +0200

    Continue English synonymy batch 367

--- js/dictionary-manifest.js
+++ js/dictionary-manifest.js
@@ -12,11 +12,11 @@
  */
 window.SINONIMIA_DICTIONARY_SHARDS = {
   es: [
-    { file: "js/data.es.js", src: "js/data.es.js?v=cbfca53ba3" },
-    { file: "js/data.es.2.js", src: "js/data.es.2.js?v=88abdf9a58" },
+    { file: "js/data.es.js", src: "js/data.es.js?v=f4fe565325" },
+    { file: "js/data.es.2.js", src: "js/data.es.2.js?v=ccbd85fdef" },
   ],
   en: [
     { file: "js/data.en.js", src: "js/data.en.js?v=6dfdbdccee" },
-    { file: "js/data.en.2.js", src: "js/data.en.2.js?v=37298b5290" },
+    { file: "js/data.en.2.js", src: "js/data.en.2.js?v=be344708f0" },
   ],
 };
