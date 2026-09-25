858d2c87590aa86f807dd9fc704a2fbbbb5986e8
Sinonimia Deploy <deploy@noreply.localhost>
2026-09-25 14:28:27 +0200

    Continue reviewed English synonymy batch 377

--- js/dictionary-manifest.js
+++ js/dictionary-manifest.js
@@ -12,11 +12,11 @@
  */
 window.SINONIMIA_DICTIONARY_SHARDS = {
   es: [
-    { file: "js/data.es.js", src: "js/data.es.js?v=158cd7274d" },
-    { file: "js/data.es.2.js", src: "js/data.es.2.js?v=43c8aad5fe" },
+    { file: "js/data.es.js", src: "js/data.es.js?v=542583e522" },
+    { file: "js/data.es.2.js", src: "js/data.es.2.js?v=c1e65ac330" },
   ],
   en: [
     { file: "js/data.en.js", src: "js/data.en.js?v=6dfdbdccee" },
-    { file: "js/data.en.2.js", src: "js/data.en.2.js?v=e9229a230e" },
+    { file: "js/data.en.2.js", src: "js/data.en.2.js?v=d38868e3cc" },
   ],
 };
