3aab9bec43f57fa01ecb6bf26da9551db56d99cb
Sinonimia Deploy <deploy@noreply.localhost>
2026-09-25 17:28:16 +0200

    Add frequent vocabulary batch 384

--- js/dictionary-manifest.js
+++ js/dictionary-manifest.js
@@ -12,11 +12,11 @@
  */
 window.SINONIMIA_DICTIONARY_SHARDS = {
   es: [
-    { file: "js/data.es.js", src: "js/data.es.js?v=c4ac4693a4" },
-    { file: "js/data.es.2.js", src: "js/data.es.2.js?v=77739f8a76" },
+    { file: "js/data.es.js", src: "js/data.es.js?v=d8c3da7048" },
+    { file: "js/data.es.2.js", src: "js/data.es.2.js?v=39cf47226c" },
   ],
   en: [
     { file: "js/data.en.js", src: "js/data.en.js?v=6dfdbdccee" },
-    { file: "js/data.en.2.js", src: "js/data.en.2.js?v=64365bf5db" },
+    { file: "js/data.en.2.js", src: "js/data.en.2.js?v=03518652cc" },
   ],
 };
