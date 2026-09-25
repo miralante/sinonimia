c16a076a13296e171a332cbcd28277fed9599001
Sinonimia Deploy <deploy@noreply.localhost>
2026-09-25 02:49:35 +0200

    Continue English synonymy batch 366

--- js/dictionary-manifest.js
+++ js/dictionary-manifest.js
@@ -12,11 +12,11 @@
  */
 window.SINONIMIA_DICTIONARY_SHARDS = {
   es: [
-    { file: "js/data.es.js", src: "js/data.es.js?v=2c57543725" },
-    { file: "js/data.es.2.js", src: "js/data.es.2.js?v=dd8d89ec42" },
+    { file: "js/data.es.js", src: "js/data.es.js?v=cbfca53ba3" },
+    { file: "js/data.es.2.js", src: "js/data.es.2.js?v=88abdf9a58" },
   ],
   en: [
     { file: "js/data.en.js", src: "js/data.en.js?v=6dfdbdccee" },
-    { file: "js/data.en.2.js", src: "js/data.en.2.js?v=09a00ef664" },
+    { file: "js/data.en.2.js", src: "js/data.en.2.js?v=37298b5290" },
   ],
 };
