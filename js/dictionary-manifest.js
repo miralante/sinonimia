ee82ec0a1b2c70b6f55651278835f2efcb2ccf47
Sinonimia Deploy <deploy@noreply.localhost>
2026-09-25 15:35:07 +0200

    Add reviewed knowledge vocabulary batch 379

--- js/dictionary-manifest.js
+++ js/dictionary-manifest.js
@@ -12,11 +12,11 @@
  */
 window.SINONIMIA_DICTIONARY_SHARDS = {
   es: [
-    { file: "js/data.es.js", src: "js/data.es.js?v=70cebc0f05" },
-    { file: "js/data.es.2.js", src: "js/data.es.2.js?v=5bdd9de4fc" },
+    { file: "js/data.es.js", src: "js/data.es.js?v=10c98317f1" },
+    { file: "js/data.es.2.js", src: "js/data.es.2.js?v=5b5e614caa" },
   ],
   en: [
     { file: "js/data.en.js", src: "js/data.en.js?v=6dfdbdccee" },
-    { file: "js/data.en.2.js", src: "js/data.en.2.js?v=54a6342362" },
+    { file: "js/data.en.2.js", src: "js/data.en.2.js?v=ee526ff5d3" },
   ],
 };
