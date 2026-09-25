419af80baedf4efa47061b6e8c662000d35fedd8
Sinonimia Deploy <deploy@noreply.localhost>
2026-09-25 17:50:18 +0200

    Add reviewed knowledge vocabulary batch 385

--- js/dictionary-manifest.js
+++ js/dictionary-manifest.js
@@ -12,11 +12,11 @@
  */
 window.SINONIMIA_DICTIONARY_SHARDS = {
   es: [
-    { file: "js/data.es.js", src: "js/data.es.js?v=d8c3da7048" },
-    { file: "js/data.es.2.js", src: "js/data.es.2.js?v=39cf47226c" },
+    { file: "js/data.es.js", src: "js/data.es.js?v=ac40042a98" },
+    { file: "js/data.es.2.js", src: "js/data.es.2.js?v=66b851c697" },
   ],
   en: [
     { file: "js/data.en.js", src: "js/data.en.js?v=6dfdbdccee" },
-    { file: "js/data.en.2.js", src: "js/data.en.2.js?v=03518652cc" },
+    { file: "js/data.en.2.js", src: "js/data.en.2.js?v=76cb23ba67" },
   ],
 };
