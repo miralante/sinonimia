9686c767bf6a8e9f783c27612c05f2a392e0994c
Sinonimia Deploy <deploy@noreply.localhost>
2026-09-25 13:17:20 +0200

    Continue reviewed English synonymy batch 375

--- js/dictionary-manifest.js
+++ js/dictionary-manifest.js
@@ -12,11 +12,11 @@
  */
 window.SINONIMIA_DICTIONARY_SHARDS = {
   es: [
-    { file: "js/data.es.js", src: "js/data.es.js?v=d9c3ec7380" },
-    { file: "js/data.es.2.js", src: "js/data.es.2.js?v=732b170059" },
+    { file: "js/data.es.js", src: "js/data.es.js?v=878a6cef33" },
+    { file: "js/data.es.2.js", src: "js/data.es.2.js?v=43c8aad5fe" },
   ],
   en: [
     { file: "js/data.en.js", src: "js/data.en.js?v=6dfdbdccee" },
-    { file: "js/data.en.2.js", src: "js/data.en.2.js?v=141d18f046" },
+    { file: "js/data.en.2.js", src: "js/data.en.2.js?v=3f4e3be806" },
   ],
 };
