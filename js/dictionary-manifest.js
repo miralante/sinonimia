121ff03216566af7372e21981c54c9473d79da5b
Sinonimia Deploy <deploy@noreply.localhost>
2026-09-25 15:48:08 +0200

    Add reviewed vocabulary batch 380

--- js/dictionary-manifest.js
+++ js/dictionary-manifest.js
@@ -12,11 +12,11 @@
  */
 window.SINONIMIA_DICTIONARY_SHARDS = {
   es: [
-    { file: "js/data.es.js", src: "js/data.es.js?v=10c98317f1" },
-    { file: "js/data.es.2.js", src: "js/data.es.2.js?v=5b5e614caa" },
+    { file: "js/data.es.js", src: "js/data.es.js?v=78ad30b057" },
+    { file: "js/data.es.2.js", src: "js/data.es.2.js?v=c77e72a368" },
   ],
   en: [
     { file: "js/data.en.js", src: "js/data.en.js?v=6dfdbdccee" },
-    { file: "js/data.en.2.js", src: "js/data.en.2.js?v=ee526ff5d3" },
+    { file: "js/data.en.2.js", src: "js/data.en.2.js?v=c4209d72d3" },
   ],
 };
