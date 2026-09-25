980d9b662ca6264c87ca025234594e3e1387f22c
Sinonimia Deploy <deploy@noreply.localhost>
2026-09-25 13:05:14 +0200

    Continue reviewed English synonymy batch 374

--- js/dictionary-manifest.js
+++ js/dictionary-manifest.js
@@ -12,11 +12,11 @@
  */
 window.SINONIMIA_DICTIONARY_SHARDS = {
   es: [
-    { file: "js/data.es.js", src: "js/data.es.js?v=cdc9c9bd50" },
+    { file: "js/data.es.js", src: "js/data.es.js?v=d9c3ec7380" },
     { file: "js/data.es.2.js", src: "js/data.es.2.js?v=732b170059" },
   ],
   en: [
     { file: "js/data.en.js", src: "js/data.en.js?v=6dfdbdccee" },
-    { file: "js/data.en.2.js", src: "js/data.en.2.js?v=0bd9413964" },
+    { file: "js/data.en.2.js", src: "js/data.en.2.js?v=141d18f046" },
   ],
 };
